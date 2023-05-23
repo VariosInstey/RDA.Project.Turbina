fb5cfbb72900e13a27994dc240686a974b613c3b
Пользователь <@DESKTOP-KVSP5S9>
2023-05-23 17:40:10 +0300

    Добавьте файлы проекта.

---
+++ View/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using RDA.Project.Turbina.Core;
+using RDA.Project.Turbina.View.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RDA.Project.Turbina;
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        CoreNavigate.Connection = MainFrame;
+        MainFrame.Navigate(new MainPage());
+    }
+
+    private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+    {
+        if (e.ClickCount == 2 && this.WindowState == WindowState.Normal) 
+        {
+            this.WindowState = WindowState.Maximized;
+        }
+        else if (e.ClickCount == 2 && this.WindowState == WindowState.Maximized)
+        {
+            this.WindowState = WindowState.Normal;
+        }
+    }
+
+    private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+    {
+        if(e.ChangedButton == MouseButton.Left)
+        {
+            DragMove();
+        }
+    }
+}
---
+++ View/Pages/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using RDA.Project.Turbina.Core;
+using RDA.Project.Turbina.View.Pages.Reg_Login;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RDA.Project.Turbina.View.Pages;
+/// <summary>
+/// Логика взаимодействия для MainPage.xaml
+/// </summary>
+public partial class MainPage : Page
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void BtnGuest_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void Reg_Click(object sender, RoutedEventArgs e)
+    {
+        CoreNavigate.Connection?.Navigate(new LoginPage());
+    }
+
+    private void Register_Click(object sender, RoutedEventArgs e)
+    {
+        CoreNavigate.Connection?.Navigate(new RegPage());
+    }
+}
